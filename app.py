3e8a2be40edc2213e176f2e46b7d692b086fdd87
Igrojas <igrojasro@hotmail.com>
2024-07-29 23:59:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,10 @@ import streamlit as st
 import pandas as pd
 from utils import *
 
-st.set_page_config(page_title="Top semanal Chile", layout="wide")
+st.set_page_config(page_title="Top semanal Chile",
+                   layout="wide")
+
+
 
 df = pd.read_csv("data/regional-cl-weekly-2024-07-25.csv", sep=",")
 first_artist = []
@@ -31,7 +34,7 @@ with st.container():
         st.markdown(f"""
 
                 <div style="text-align: center;">
-                    <h2 style="font-weight: bold;">Artista más escuchado</h2>
+                    <h2 style="font-weight: bold; color: #b3b3b3;font-family: sans-serif;">Artista más escuchado</h2>
                     <span style="font-size: 35px; color: #1DB954; font-weight: bold;">
                     {primer_artista_nombre}
                     </span>
@@ -41,7 +44,7 @@ with st.container():
     with col2:
             st.markdown(f"""
                     <div style="text-align: center;">
-                        <h2 style="font-weight: bold;">Número de reproducciones</h2>
+                        <h2 style="font-weight: bold; color: #b3b3b3; font-family: sans-serif;">Número de reproducciones</h2>
                         <span style="font-size: 35px; color: #1DB954; font-weight: bold;">
                         {primer_artista_streams:,}
                         </span>
@@ -62,7 +65,7 @@ with st.container():
         st.markdown(f"""
 
                 <div style="text-align: center;">
-                    <h2 style="font-weight: bold;">Artista con más semanas en el top</h2>
+                    <h2 style="font-weight: bold; color: #b3b3b3; font-family: sans-serif;">Artista con más semanas en el top</h2>
                     <span style="font-size: 35px; color: #1DB954; font-weight: bold;">
                     {primer_artista_nombre}
                     </span>
@@ -72,7 +75,7 @@ with st.container():
     with col1:
             st.markdown(f"""
                     <div style="text-align: center;">
-                        <h2 style="font-weight: bold;">Número de semanas en el top</h2>
+                        <h2 style="font-weight: bold; color: #b3b3b3; font-family: sans-serif;">Número de semanas en el top</h2>
                         <span style="font-size: 35px; color: #1DB954; font-weight: bold;">
                         {primer_artista_weeks:,}
                         </span>
@@ -81,7 +84,7 @@ with st.container():
             
     with col2:
          st.altair_chart(pie_top_weeked, use_container_width=True)
-         
+
     with col3:
         st.altair_chart(chart_top_weeks, use_container_width=True)
 
